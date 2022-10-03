040c2d3d3b36eea559add22806e0cfdf8b6179dd
Paul Wheeler <pwheeler@revacomm.com>
2022-10-02 22:51:15 -1000

    WIP: switch to build of fluent-bit with logging and a bug fix for the native loki plugin.

--- grafana-loki-plugin-build/build-and-deploy.sh
+++ grafana-loki-plugin-build/build-and-deploy.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 
-docker build . -t ghcr.io/revacomm-pwheeler/fluent-bit-grafana-loki:latest
-docker push ghcr.io/revacomm-pwheeler/fluent-bit-grafana-loki:latest
+docker build . -t ghcr.io/revacomm-pwheeler/fluent-bit-loki-grafana/fluent-bit-grafana-plugin:latest
+docker push ghcr.io/revacomm-pwheeler/fluent-bit-loki-grafana/fluent-bit-grafana-plugin:latest
